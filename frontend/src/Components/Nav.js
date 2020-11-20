c63aacfd2fe438a6707ee7f497be3e85f9073cd8
yezii <1126yezy@gmail.com>
2020-11-20 23:12:53 +0900

    Add media query and update Info, Detail

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,107 +1,91 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVirus, faFileAlt, faMapMarkerAlt, faInfoCircle, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faVirus,
+  faFileAlt,
+  faMapMarkerAlt,
+  faInfoCircle,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import "../Components/css/styles.css";
 import Position from "./Position";
+import Modal from "./Modal";
 
 function Nav() {
   const [dateTime, setDateTime] = useState(new Date());
   const [displayModal, setDisplayModal] = useState(false);
 
   useEffect(() => {
-      const id = setInterval(() => setDateTime(new Date()), 1000);
-      return () => {
-          clearInterval(id);
-      }
-    }, []);
+    const id = setInterval(() => setDateTime(new Date()), 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
-  return(
-      <div className="home__nav">
-        <div className="nav__title">
+  return (
+    <div className="home__nav">
+      <div className="nav__title">
+        <Router>
+          <Link to="/" className="text-link">
+            <div className="nav__icon">
+              <FontAwesomeIcon icon={faVirus} className="nav-icon" />
+            </div>
+            <div className="nav__listTitle">코로나오늘</div>
+          </Link>
+        </Router>
+      </div>
+      <div className="nav__date">{`${dateTime.toLocaleDateString()}`}</div>
+      <div className="nav__city">
+        <FontAwesomeIcon icon={faMapMarkerAlt} className="nav-icon-map" />
+        <Position />
+      </div>
+
+      <ul className="nav__lists">
+        <li className="nav__list">
           <Router>
             <Link to="/" className="text-link">
-              <div className="nav__icon"><FontAwesomeIcon icon={faVirus} className="nav-icon" /></div>
-              <div className="nav__listTitle">코로나오늘</div>
+              <div className="nav__icon">
+                <FontAwesomeIcon icon={faFileAlt} className="nav-icon" />
+              </div>
+              <div className="nav__listTitle">코로나 현황</div>
             </Link>
           </Router>
-        </div>
-        <div className="nav__date">
-          {`${dateTime.toLocaleDateString()}`}
-        </div>
-        <div className="nav__city">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="nav-icon-map"/>
-          <Position />
-        </div>
+        </li>
+        <li className="nav__list">
+          <Router>
+            <Link to="/info" className="text-link">
+              <div className="nav__icon">
+                <FontAwesomeIcon icon={faInfoCircle} className="nav-icon" />
+              </div>
+              <div className="nav__listTitle">코로나 인포</div>
+            </Link>
+          </Router>
+        </li>
+      </ul>
+      <div className="nav__footer">
+        <p>Copyright © 2020 코로나오늘</p>
+        <span
+          className="nav__github"
+          onClick={() =>
+            window.open("https://github.com/LeeHyeSu/coronaonul", "_blank")
+          }
+        >
+          Github
+        </span>
+      </div>
 
-        <ul className="nav__lists">
-          <li className="nav__list">
-            <Router>
-              <Link to="/" className="text-link">
-                <div className="nav__icon"><FontAwesomeIcon icon={faFileAlt} className="nav-icon"/></div>
-                <div className="nav__listTitle">코로나 현황</div>
-              </Link>
-            </Router>
-          </li>
-          <li className="nav__list">
-            <Router>
-              <Link to="/info" className="text-link">
-                <div className="nav__icon"><FontAwesomeIcon icon={faInfoCircle} className="nav-icon"/></div>
-                <div className="nav__listTitle">코로나 인포</div>
-              </Link>
-            </Router>
-          </li>
-        </ul>
-        <div className="nav__footer">
-          <p>Copyright © 2020 코로나오늘</p>
-          <span className="nav__github" onClick={() => window.open("https://github.com/LeeHyeSu/coronaonul", "_blank")}>Github</span>
-        </div>
+      {/* Modal */}
 
-        {/* Modal */}
+      <button
+        className="btnModal"
+        onClick={() => setDisplayModal(!displayModal)}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
 
-        <button className="btnModal" onClick={() => setDisplayModal(!displayModal)}>
-          <FontAwesomeIcon icon={faBars}/>
-        </button>
-
-        <div className={`Modal ${displayModal ? "Show" : ""}`}>
-          <button className="btnClose" onClick={() => setDisplayModal(!displayModal)}>
-          <FontAwesomeIcon icon={faTimes}/>
-          </button>
-
-          <h3 className="modalTitle">코로나 오늘</h3>
-
-          <div className={`modalText ${displayModal ? "Show" : ""}`}>
-            <div className="modal-nav__date">
-              {`${dateTime.toLocaleDateString()}`}
-            </div>
-            <div className="modal-nav__city">
-              <FontAwesomeIcon icon={faMapMarkerAlt} className="nav-icon-map"/>
-              <Position />
-            </div>
-            <ul className="modal-nav__lists">
-              <li className="modal-nav__list">
-                <Router>
-                  <Link to="/" className="text-link">
-                    <div className="nav__icon"><FontAwesomeIcon icon={faFileAlt} className="nav-icon"/></div>
-                    <div className="nav__listTitle">코로나 현황</div>
-                  </Link>
-                </Router>
-              </li>
-              <li className="modal-nav__list">
-                <Router>
-                  <Link to="/info" className="text-link">
-                    <div className="nav__icon"><FontAwesomeIcon icon={faInfoCircle} className="nav-icon"/></div>
-                    <span className="nav__listTitle">코로나 인포</span>
-                  </Link>
-                </Router>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          className={`Overlay ${displayModal ? "Show" : ""}`}
-          onClick={() => setDisplayModal(!displayModal)}
-        />
+      <Modal />
     </div>
   );
 }
--- frontend/src/Components/Position.js
+++ frontend/src/Components/Position.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import dotenv from 'dotenv';
+import React from "react";
+import dotenv from "dotenv";
 
 dotenv.config();
+// require("dotenv").config();
 
 class Position extends React.Component {
   state = {
     lat: undefined,
-    lon: undefined,  
+    lon: undefined,
     // tempC: undefined,
     city: undefined,
     country: undefined,
-    errorMessage: undefined
+    errorMessage: undefined,
   };
-  
+
   componentDidMount() {
     if (navigator.geolocation) {
       this.getPosition()
-      .then((position) => {      
-        this.getWeather(position.coords.latitude, position.coords.longitude)
-      })
-      .catch((err) => {
-        this.setState({ errorMessage: err.message });
-      });
-    }
-    else {
+        .then((position) => {
+          this.getWeather(position.coords.latitude, position.coords.longitude);
+        })
+        .catch((err) => {
+          this.setState({ errorMessage: err.message });
+        });
+    } else {
       alert("Geolocation not available");
     }
     this.timerID = setInterval(
@@ -35,41 +35,42 @@ class Position extends React.Component {
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
-  
+
   getPosition = (options) => {
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
-  }
+  };
 
-  getWeather = async (lat, lon) => {     
-    const api_call = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f3c59f07aa3c3d51508dd682e2c66808&units=metric`);
+  getWeather = async (lat, lon) => {
+    const api_call = await fetch(
+      `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API}&units=metric`
+    );
     const data = await api_call.json();
     this.setState({
       lat: lat,
       lon: lon,
       city: data.name,
       // tempC: Math.round(data.main.temp),
-    })
-  }
-  
+    });
+  };
+
   render() {
     if (this.state.city) {
       return (
-        <div className="weather">   
+        <div className="weather">
           <div>
-            <span className="weather-item">{this.state.city}</span> 
+            <span className="weather-item">{this.state.city}</span>
             <span className="weather-item">
               {/* {this.state.tempC} &deg;C  */}
-            </span>            
+            </span>
           </div>
-        </div>         
-      )
-    }
-    else {
+        </div>
+      );
+    } else {
       return null;
     }
   }
-} 
-  
+}
+
 export default Position;
